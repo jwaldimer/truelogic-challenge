5f89e6062bdb8c8277fef18a236141f2d95f07db
jwaldimer <jwaldimer@gmail.com>
2024-08-30 18:15:39 -0500

    remove blank space

--- challenge.rb
+++ challenge.rb
@@ -2,7 +2,6 @@
 # Download this text file, then write a program to output the day number (column one)
 # with the smallest temperature spread (the maximum temperature is the second column,
 # the minimum the third column).
-
 def smallest_temperature_spread
   lines = read_and_clean_file('w_data.dat')
   spreads = []
@@ -23,7 +22,6 @@ end
 # against each team in that season (so Arsenal scored 79 goals against opponents,
 # and had 36 goals scored against them). Write a program to print the name of the team
 # with the smallest difference in ‘for’ and ‘against’ goals.
-
 def smallest_goals_difference
   lines = read_and_clean_file('soccer.dat')
   differences = []
